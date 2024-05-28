ed1a2cbea9d4c95cbd0698c547276bfa2f680201
yagvendrajoshi <yagvendrajoshi@gmail.com>
2024-05-28 09:45:48 +0200

    addded two files for transform

---
+++ assets/sql/transform/all_detail.sql
@@ -0,0 +1,26 @@
+select
+
+    game_id,
+    game_url,
+    game_mode,
+    start_date,
+    username,
+    user_color,
+    user_rating,
+    user_accuracy,
+    opponent,
+    opponent_rating,
+    opponent_accuracy,
+    rating_diff,
+    match_result,
+    result_subcategory,
+    start_date_time,
+    end_date_time,
+    game_duration,
+    game_duration_sec,
+    rounds,
+    user_avg_move_time_sec,
+    opening
+    
+from
+    games

c55bd0d8961804804a007f414a86c8e449692d25
danihello <danihello@gmail.com>
2024-05-28 17:20:07 +0300

    Added_logging

--- pipelines/Chess.py
+++ pipelines/Chess.py
@@ -10,16 +10,17 @@ from sqlalchemy import Table, MetaData, Column, Integer, String, Float, BigInteg
 from assets.Chess import extract_eco_codes, extract_games, extract_user_info, load, incremental_modify_dates, transform
 from connectors.Chess import ChessApiClient
 from connectors.postgresql import PostgreSqlClient
+from assets.pipeline_logging import PipelineLogging
+from assets.metadata_logging import MetaDataLoggingStatus, MetaDataLogging
 
 if __name__ == "__main__":
     # setting up environment variables
     load_dotenv()
-    USER_AGENT = os.environ.get("USER_AGENT")
-    SERVER_NAME = os.environ.get("SERVER_NAME")
-    DATABASE_NAME = os.environ.get("DATABASE_NAME")
-    DB_USERNAME = os.environ.get("DB_USERNAME")
-    DB_PASSWORD = os.environ.get("DB_PASSWORD")
-    PORT = os.environ.get("PORT")
+    LOGGING_SERVER_NAME = os.environ.get("LOGGING_SERVER_NAME")
+    LOGGING_DATABASE_NAME = os.environ.get("LOGGING_DATABASE_NAME")
+    LOGGING_USERNAME = os.environ.get("LOGGING_USERNAME")
+    LOGGING_PASSWORD = os.environ.get("LOGGING_PASSWORD")
+    LOGGING_PORT = os.environ.get("LOGGING_PORT")
 
     # get config file
     yaml_file_path = __file__.replace(".py", ".yaml")
@@ -32,132 +33,176 @@ if __name__ == "__main__":
             f"Missing {yaml_file_path} file! Please create the yaml file with at least a `name` key for the pipeline name."
         )
 
-    # extracting variables from config file
-
-    start_date = pipeline_config.get("config").get("games").get("start_date")
-    end_date = pipeline_config.get("config").get("games").get("end_date")
-    target_table_games = pipeline_config.get("config").get("games").get("target_table")
-    target_column = pipeline_config.get("config").get("games").get("target_column")
-    usernames = pipeline_config.get("config").get("games").get("usernames")
-
-    # extracting players from config, either from players section or from games section (if players section is missing/empty)
-    players = pipeline_config.get("config").get("players").get("usernames")
-    if players is None:
-        players = pipeline_config.get("config").get("games").get("username")
-
-    target_table_players = pipeline_config.get("config").get("players").get("target_table")
-
-
-    # defining postrgesql client
-    postgres_sql_client = PostgreSqlClient(server_name=SERVER_NAME,
-                    database_name=DATABASE_NAME,
-                    username=DB_USERNAME,
-                    password=DB_PASSWORD,
-                    port=PORT)
-    metadata = MetaData()
-
-    # TODO - add check for the availability of the postgres instance
-
-    games_tbl = Table("games",
-            metadata,
-            Column('game_id',BigInteger, primary_key=True),
-            Column('game_url', String),
-            Column('game_mode', String),
-            Column('start_date', String),
-            Column('username', String),
-            Column('user_color', String),
-            Column('user_rating', Integer),
-            Column('user_accuracy', Float),
-            Column('opponent', String),
-            Column('opponent_rating', Integer),
-            Column('opponent_accuracy', Float),
-            Column('rating_diff', Integer),
-            Column('match_result', String),
-            Column('result_subcategory', String),
-            Column('start_date_time', String),
-            Column('end_date_time', String),
-            Column('game_duration', String),
-            Column('game_duration_sec', Integer),
-            Column('rounds', Integer),
-            Column('user_avg_move_time_sec', Float),
-            Column('opening', String)
-            )
-    eco_codes = extract_eco_codes(pipeline_config.get("config").get("eco_codes_path"))
-
-    for username in usernames:
-        chess_api_client = ChessApiClient(username, USER_AGENT)
-        print(f"getting games for user: {chess_api_client.username}")
-        # run this "incremental_modify_dates" function to check if the username exists, if so the start date will update to one day ahead of max date
-        # end date will evaluate to current date
-        start_date, end_date = incremental_modify_dates(ChessApiClient=chess_api_client,
-                                                        PostgreSqlClient=postgres_sql_client,
-                                                        target_table=target_table_games,
-                                                        target_column=target_column,
-                                                        start_date=start_date,
-                                                        end_date=end_date)
-        # extract
-        valid_games = extract_games(start_date=start_date,
-                    end_date=end_date,
-                    chess_api_client=chess_api_client)
-        if valid_games.shape[0] > 0:
-            #transform
-            trasformed_games = transform(valid_games, eco_codes)
-            #load
-            load(df=trasformed_games,
-                postgresql_client=postgres_sql_client,
-                table=games_tbl,
-                metadata=metadata,
-                load_method="insert")
-    # players
-
-    # defining target table
-    players_tbl = Table("players",
-        metadata,
-        Column('player_id', BigInteger, primary_key=True),
-        Column('snaphot_date', TIMESTAMP, default=datetime.now(), primary_key=True),
-        Column('name',String),
-        Column('username', String),
-        Column('title', String),
-        Column('followers', BigInteger),
-        Column('country', String),
-        Column('location', String),
-        Column('last_online', BigInteger),
-        Column('joined', BigInteger),
-        Column('is_streamer', Boolean)
+    # defining logger
+    pipeline_logging = PipelineLogging(
+        pipeline_name=pipeline_config.get("name"),
+        log_folder_path=pipeline_config.get('config').get("log_folder_path"),
     )
 
-    # looping through players in config file
-    # making sure players is a list to iretare through
-    if not isinstance(players, list):
-        players = [players]
+    #defining postgres sql for logging storage
+    postgresql_logging_client = PostgreSqlClient(
+        server_name=LOGGING_SERVER_NAME,
+        database_name=LOGGING_DATABASE_NAME,
+        username=LOGGING_USERNAME,
+        password=LOGGING_PASSWORD,
+        port=LOGGING_PORT,
+    )
 
-    for username in players:
+    metadata_logger = MetaDataLogging(
+        pipeline_name=PIPLINE_NAME,
+        postgresql_client=postgresql_logging_client,
+        config=pipeline_config.get('config'),
+    )
+    try:
+        metadata_logger.log()
+        # extracting variables from config file
+        pipeline_logging.logger.info("Starting pipeline run")
+        pipeline_logging.logger.info("Getting pipeline environment variables")
+        USER_AGENT = os.environ.get("USER_AGENT")
+        SERVER_NAME = os.environ.get("SERVER_NAME")
+        DATABASE_NAME = os.environ.get("DATABASE_NAME")
+        DB_USERNAME = os.environ.get("DB_USERNAME")
+        DB_PASSWORD = os.environ.get("DB_PASSWORD")
+        PORT = os.environ.get("PORT")
+        start_date = pipeline_config.get("config").get("games").get("start_date")
+        end_date = pipeline_config.get("config").get("games").get("end_date")
+        target_table_games = pipeline_config.get("config").get("games").get("target_table")
+        target_column = pipeline_config.get("config").get("games").get("target_column")
+        usernames = pipeline_config.get("config").get("games").get("usernames")
 
-        chess_api_client = ChessApiClient(username, user_agent=USER_AGENT)
+        # extracting players from config, either from players section or from games section (if players section is missing/empty)
+        players = pipeline_config.get("config").get("players").get("usernames")
+        if players is None:
+            players = pipeline_config.get("config").get("games").get("username")
 
-        # extract player info
-        player_df = extract_user_info(chess_api_client=chess_api_client)
+        target_table_players = pipeline_config.get("config").get("players").get("target_table")
 
-        # transform player (adding missing columns if needed)
-        player_df = player_df.reindex(columns=['player_id',
-                               'name',
-                               'username',
-                               'title',
-                               'followers',
-                               'country',
-                               'location',
-                               'last_online',
-                               'joined',
-                               'is_streamer'])
 
-        #load player
-        load(df=player_df,
-            postgresql_client=postgres_sql_client,
-            table=players_tbl,
-            metadata=metadata,
-            load_method="insert"
+        # defining postrgesql client
+        postgres_sql_client = PostgreSqlClient(server_name=SERVER_NAME,
+                        database_name=DATABASE_NAME,
+                        username=DB_USERNAME,
+                        password=DB_PASSWORD,
+                        port=PORT)
+        metadata = MetaData()
+
+        # TODO - add check for the availability of the postgres instance
+
+        games_tbl = Table("games",
+                metadata,
+                Column('game_id',BigInteger, primary_key=True),
+                Column('game_url', String),
+                Column('game_mode', String),
+                Column('start_date', String),
+                Column('username', String),
+                Column('user_color', String),
+                Column('user_rating', Integer),
+                Column('user_accuracy', Float),
+                Column('opponent', String),
+                Column('opponent_rating', Integer),
+                Column('opponent_accuracy', Float),
+                Column('rating_diff', Integer),
+                Column('match_result', String),
+                Column('result_subcategory', String),
+                Column('start_date_time', String),
+                Column('end_date_time', String),
+                Column('game_duration', String),
+                Column('game_duration_sec', Integer),
+                Column('rounds', Integer),
+                Column('user_avg_move_time_sec', Float),
+                Column('opening', String)
+                )
+        eco_codes = extract_eco_codes(pipeline_config.get("config").get("eco_codes_path"))
+        pipeline_logging.logger.info('Begining Games ETL')
+        for username in usernames:
+            chess_api_client = ChessApiClient(username, USER_AGENT)
+            # run this "incremental_modify_dates" function to check if the username exists, if so the start date will update to one day ahead of max date
+            # end date will evaluate to current date
+            start_date, end_date = incremental_modify_dates(ChessApiClient=chess_api_client,
+                                                            PostgreSqlClient=postgres_sql_client,
+                                                            target_table=target_table_games,
+                                                            target_column=target_column,
+                                                            start_date=start_date,
+                                                            end_date=end_date)
+            # extract
+            pipeline_logging.logger.info(f'Extracting data from Chess API games: username: {chess_api_client.username}, start_date: {start_date}, end_date: {end_date}')
+            valid_games = extract_games(start_date=start_date,
+                        end_date=end_date,
+                        chess_api_client=chess_api_client)
+            if valid_games.shape[0] > 0:
+                #transform
+                pipeline_logging.logger.info('Trasforming dataframes')
+                trasformed_games = transform(valid_games, eco_codes)
+                #load
+                pipeline_logging.logger.info('Loading data to postgres')
+                load(df=trasformed_games,
+                    postgresql_client=postgres_sql_client,
+                    table=games_tbl,
+                    metadata=metadata,
+                    load_method="upsert")
+        pipeline_logging.logger.info('Games ETL run successful')
+        # players
+
+        # defining target table
+        players_tbl = Table("players",
+            metadata,
+            Column('player_id', BigInteger, primary_key=True),
+            Column('snaphot_date', TIMESTAMP, default=datetime.now(), primary_key=True),
+            Column('name',String),
+            Column('username', String),
+            Column('title', String),
+            Column('followers', BigInteger),
+            Column('country', String),
+            Column('location', String),
+            Column('last_online', BigInteger),
+            Column('joined', BigInteger),
+            Column('is_streamer', Boolean)
         )
 
+        # looping through players in config file
+        # making sure players is a list to iretare through
+        if not isinstance(players, list):
+            players = [players]
+        pipeline_logging.logger.info('Begining players ETL')
+        for username in players:
+
+            chess_api_client = ChessApiClient(username, user_agent=USER_AGENT)
+
+            # extract player info
+            pipeline_logging.logger.info(f'Extracting data from Chess API users: username: {chess_api_client.username}')
+            player_df = extract_user_info(chess_api_client=chess_api_client)
+
+            # transform player (adding missing columns if needed)
+            pipeline_logging.logger.info('Trasforming dataframes')
+            player_df = player_df.reindex(columns=['player_id',
+                                'name',
+                                'username',
+                                'title',
+                                'followers',
+                                'country',
+                                'location',
+                                'last_online',
+                                'joined',
+                                'is_streamer'])
+
+            #load player
+            pipeline_logging.logger.info('Loading data to postgres')
+            load(df=player_df,
+                postgresql_client=postgres_sql_client,
+                table=players_tbl,
+                metadata=metadata,
+                load_method="insert"
+            )
+            pipeline_logging.logger.info("Loading data to postgres")
+        pipeline_logging.logger.info("Players ETL run successful")
+        metadata_logger.log(
+            status=MetaDataLoggingStatus.RUN_SUCCESS, logs=pipeline_logging.get_logs()
+        )  # log end
+    except Exception as e:
+        pipeline_logging.logger.error(f"Pipeline run failed. See detailed logs: {e}")
+        metadata_logger.log(
+            status=MetaDataLoggingStatus.RUN_FAILURE, logs=pipeline_logging.get_logs()
+        )  #
 
 
 

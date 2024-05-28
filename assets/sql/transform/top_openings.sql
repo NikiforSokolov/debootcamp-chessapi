11105ddb411079019a7e98fc132acdbc73fde467
yagvendrajoshi <yagvendrajoshi@gmail.com>
2024-05-28 17:02:27 +0200

    added the query in the top_opening

---
+++ assets/sql/transform/top_openings.sql
@@ -0,0 +1,12 @@
+SELECT 
+    username,
+    opening,
+    game_mode,
+    COUNT(CASE WHEN match_result = 'win' THEN 1 END) AS no_of_win,
+    COUNT(CASE WHEN match_result = 'defet' THEN 1 END) AS no_of_defeat,
+    COUNT(CASE WHEN match_result = 'draw' THEN 1 END) AS no_of_draw
+FROM 
+    games
+GROUP BY 
+    opening,username,game_mode
+
